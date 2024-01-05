0ba0eac7a986674c0d7f00a24a8bc0411b6d7946
Jahongir04 <jahongirachilov004@gmail.com>
2024-01-05 14:47:21 +0500

    sorting

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,16 +1,25 @@
 package com.epam.mjc.collections.list;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ListSorter {
+
     public void sort(List<String> sourceList) {
+        Integer[] numericNumbers=new Integer[sourceList.size()];
+        for (int i = 0; i < sourceList.size(); i++) {
+            numericNumbers[i] = Integer.parseInt(sourceList.get(i));
+        }
+        Arrays.sort(numericNumbers);
+        ArrayList<String> expectedList = new ArrayList<>();
+        for (int i = 0; i < numericNumbers.length; i++) {
+            expectedList.add(i,Integer.toString(numericNumbers[i]));
+        }
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
-        return 0;
+        return Integer.valueOf(a);
     }
 }
