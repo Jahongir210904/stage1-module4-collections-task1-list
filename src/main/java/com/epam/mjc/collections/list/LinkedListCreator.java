d72dc25658941aa7b58be2118de28836bce8fd79
Jahongir04 <jahongirachilov004@gmail.com>
2024-01-05 14:18:31 +0500

    some logics fixed

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,7 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> linkedList=new LinkedList<>();
+        return linkedList;
     }
 }
