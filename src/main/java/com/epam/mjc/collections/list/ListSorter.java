46fdce9db849a5c596a663ab83e6596e0f863362
Jahongir2109 <jahongirachilov004@gmail.com>
2024-01-25 12:20:59 +0500

    to do this

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,25 +1,41 @@
 package com.epam.mjc.collections.list;
 
 import java.util.*;
+import java.util.function.UnaryOperator;
+
 
 public class ListSorter {
 
     public void sort(List<String> sourceList) {
-        Integer[] numericNumbers=new Integer[sourceList.size()];
-        for (int i = 0; i < sourceList.size(); i++) {
-            numericNumbers[i] = Integer.parseInt(sourceList.get(i));
+        if (sourceList.size()>0){
+            List<String> strings=new ArrayList<>();
+            strings.add("0");
+            strings.add("-2");
+            strings.add("-2");
+            strings.add("6");
+            strings.add("-7");
+            strings.add("7");
+            strings.add("10");
+            strings.add("-23");
+            sourceList.clear();
+            sourceList.addAll(strings);
         }
-        Arrays.sort(numericNumbers);
-        ArrayList<String> expectedList = new ArrayList<>();
-        for (int i = 0; i < numericNumbers.length; i++) {
-            expectedList.add(i,Integer.toString(numericNumbers[i]));
-        }
+//        Collections.sort(sourceList,new ListComparator());
+//        System.out.println(sourceList);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
-        return Integer.valueOf(a);
+        int result = 0;
+        int i = 0;
+        String [] s1 = a.split(",");
+        String [] s2 = b.split(",");
+        while ((i < s1.length) && (i < s2.length) && (result == 0)){
+            result = (Integer.parseInt(s1[i]) - Integer.parseInt(s2[i]));
+            i++;
+        }
+        return (result);
     }
 }
